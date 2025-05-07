9fdf682e265b7f758fea9dce369339d45fd22725
E_A <46349404+123AKAk@users.noreply.github.com>
2025-05-07 15:03:59 +0100

    Test

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Test route
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'API is running' });
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use(errorHandler);
